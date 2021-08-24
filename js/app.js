dbd3d15c02cd2e4744e626edf1441c538386c5a0
Mark Goudie <goudie.mark@gmail.com>
2021-08-24 17:55:12 +1000

    js update - smooth scrolling and sticky nav

--- js/app.js
+++ js/app.js
@@ -12,12 +12,12 @@
  * JS Standard: ESlint
  *
  */
-
 /**
  * Define Global Variables
  *
  */
-
+const navList = document.getElementById("nav_list");
+const sections = document.querySelectorAll("section");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -46,6 +46,7 @@ allLinks.forEach(function (link) {
     const href = link.getAttribute("href");
 
     // Scroll back to top
+
     if (href === "#")
       window.scrollTo({
         top: 0,
@@ -53,63 +54,18 @@ allLinks.forEach(function (link) {
       });
 
     // Scroll to other links
+
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
     // Close mobile naviagtion
+
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
   });
 });
-
-// // Sticky navigation
-
-// const sectionHeroEl = document.querySelector(".section-hero");
-
-// const obs = new IntersectionObserver(
-//   function (entries) {
-//     const ent = entries[0];
-//     console.log(ent);
-
-//     if (ent.isIntersecting === false) {
-//       document.body.classList.add("sticky");
-//     }
-
-//     if (ent.isIntersecting === true) {
-//       document.body.classList.remove("sticky");
-//     }
-//   },
-//   {
-//     // In the viewport
-//     root: null,
-//     threshold: 0,
-//     rootMargin: "-80px",
-//   }
-// );
-// obs.observe(sectionHeroEl);
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
-// const myName = "Mark Goudie";
-// const h1 = document.querySelector(".heading-primary");
-
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
 
 // Make mobile navigation work
 
@@ -119,3 +75,29 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+// Sticky navigation //
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
